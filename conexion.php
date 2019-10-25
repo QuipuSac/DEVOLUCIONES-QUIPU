61ff5217deeb6b0a017c67f0658ba249b949cc61
teamLeonardo <leonardosm3.14@gmail.com>
2019-10-25 18:10:58 -0500

    verde

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@ function Conexion($basedatos){
 	//
 	$adm= 'XOSCAR';
 	$contraseña = 'QUIPU2846+*';
-	$servidor = 'SERVIDOR'; 
+	$servidor = 'quipuservidor.ddns.net'; 
 	//$basedatos = @$_POST['nempresas'];
 	//$usuario = @$_POST['nnombre'];
 	//$pass = @$_POST['npassword'];
